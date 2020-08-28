0d12aef5eba6446a3027d93762787363aa9f6b2b
Tao Du <taodu@csail.mit.edu>
2020-08-27 23:15:14 -0400

    Final refinement.py.

--- python/example/refinement.py
+++ python/example/refinement.py
@@ -23,7 +23,7 @@ control_points_upper = ndarray([
     [32, 14]
 ])
 
-def solve_forward_amplifier_2d(scale):
+def solve_forward_amplifier_2d(nu, scale):
     scene = Scene2d()
     # Initialize shape.
     scaled_cell_nums = (cell_nums[0] * scale, cell_nums[1] * scale)
@@ -35,7 +35,6 @@ def solve_forward_amplifier_2d(scale):
 
     # Initialize cell.
     E = 100
-    nu = 0.499
     threshold = 1e-3
     edge_sample_num = 2
     scene.InitializeCell(E, nu, threshold, edge_sample_num)
@@ -67,54 +66,98 @@ def solve_forward_amplifier_2d(scale):
                 u[i, j] = 0
     return u
 
+nu = [0.45, 0.47, 0.48, 0.49, 0.495, 0.499]
 scale = [1, 2, 4, 8, 16, 32]
-u = [solve_forward_amplifier_2d(s) for s in scale]
-u_norm_max = np.max(np.sqrt(np.sum(u[-1] ** 2, axis=(2,))).ravel())
+u_nu = [solve_forward_amplifier_2d(n, scale[0]) for n in nu]
+u_nu_norm_max = np.max(np.sqrt(np.sum(u_nu[-1] ** 2, axis=(2,))).ravel())
+u_scale = [solve_forward_amplifier_2d(nu[-1], s) for s in scale]
+u_scale_norm_max = np.max(np.sqrt(np.sum(u_scale[-1] ** 2, axis=(2,))).ravel())
 
 import matplotlib.pyplot as plt
 import matplotlib.colors
-fig = plt.figure(figsize=(18, 4))
 
-def plot_velocity_field(ax, u_field, s):
+plt.rc('font', size=16)
+plt.rc('axes', titlesize=16)
+plt.rc('axes', labelsize=16)
+plt.rc('xtick', labelsize=16)
+plt.rc('ytick', labelsize=16)
+plt.rc('legend', fontsize=16)
+plt.rc('figure', titlesize=16)
+
+def plot_velocity_field(ax, u_field, s, u_max):
     x_size = ndarray(np.arange(u_field.shape[0])) / s
     y_size = ndarray(np.arange(u_field.shape[1])) / s
     X, Y = np.meshgrid(x_size, y_size)
     u_norm = np.sqrt(np.sum(u_field ** 2, axis=(2,)))
     strm = ax.streamplot(X, Y, u_field[:, :, 0].T, u_field[:, :, 1].T, density=(cell_nums[0] / cell_nums[1] * 3, 3),
-        color=u_norm.T, norm=matplotlib.colors.Normalize(0, u_norm_max),
+        color=u_norm.T, norm=matplotlib.colors.Normalize(0, u_max),
         arrowstyle='->', linewidth=1.5, cmap='coolwarm')
     ax.set_xlim([0, cell_nums[0]])
     ax.set_ylim([0, cell_nums[1]])
     ax.set_xticks([])
     ax.set_yticks([])
 
-ax_low_res = fig.add_subplot(141)
-plot_velocity_field(ax_low_res, u[0], scale[0])
+# The under refinement experiment.
+fig = plt.figure(figsize=(18, 9))
+ax_low_res = fig.add_subplot(231)
+plot_velocity_field(ax_low_res, u_scale[0], scale[0], u_scale_norm_max)
+ax_low_res.set_xlabel('{} x {}'.format(cell_nums[0] * scale[0], cell_nums[1] * scale[0]))
+ax_low_res.set_aspect('equal', 'box')
 
-ax_mid_res = fig.add_subplot(142)
-plot_velocity_field(ax_mid_res, u[3], scale[3])
+ax_mid_res = fig.add_subplot(232)
+plot_velocity_field(ax_mid_res, u_scale[3], scale[3], u_scale_norm_max)
+ax_mid_res.set_xlabel('{} x {}'.format(cell_nums[0] * scale[3], cell_nums[1] * scale[3]))
+ax_mid_res.set_aspect('equal', 'box')
 
-ax_high_res = fig.add_subplot(143)
-plot_velocity_field(ax_high_res, u[-1], scale[-1])
+ax_high_res = fig.add_subplot(233)
+plot_velocity_field(ax_high_res, u_scale[-1], scale[-1], u_scale_norm_max)
+ax_high_res.set_xlabel('{} x {}'.format(cell_nums[0] * scale[-1], cell_nums[1] * scale[-1]))
+ax_high_res.set_aspect('equal', 'box')
 
-# The refinement-error plot.
-ax_error = fig.add_subplot(144)
+# The convergence experiment.
+ax_low_nu = fig.add_subplot(234)
+plot_velocity_field(ax_low_nu, u_nu[0], scale[0], u_nu_norm_max)
+ax_low_nu.set_xlabel('$\\nu$ = {}'.format(nu[0]))
+ax_low_nu.set_aspect('equal', 'box')
+
+ax_mid_nu = fig.add_subplot(235)
+plot_velocity_field(ax_mid_nu, u_nu[3], scale[0], u_nu_norm_max)
+ax_mid_nu.set_xlabel('$\\nu$ = {}'.format(nu[3]))
+ax_mid_nu.set_aspect('equal', 'box')
+
+ax_high_nu = fig.add_subplot(236)
+plot_velocity_field(ax_high_nu, u_nu[-1], scale[0], u_nu_norm_max)
+ax_high_nu.set_xlabel('$\\nu$ = {}'.format(nu[-1]))
+ax_high_nu.set_aspect('equal', 'box')
+
+fig.savefig('refinement_right.pdf')
+plt.show()
+
+fig = plt.figure(figsize=(9, 9))
+ax_error = fig.add_subplot(211)
 error = []
-for s, u_s in zip(scale, u):
-    # Compute the maximum discrepancy between u_s and u[-1]
+for s, u_s in zip(scale, u_scale):
+    # Compute the maximum discrepancy.
     max_error_s = -np.inf
     for i in range(u_s.shape[0]):
         for j in range(u_s.shape[1]):
             ii = int(i * (scale[-1] / s))
             jj = int(j * (scale[-1] / s))
-            error_ij = np.max(np.abs(u_s[i, j] - u[-1][ii, jj])) / u_norm_max
+            error_ij = np.max(np.abs(u_s[i, j] - u_scale[-1][ii, jj])) / u_scale_norm_max
             if error_ij > max_error_s:
                 max_error_s = error_ij
     error.append(max_error_s)
-ax_error.plot(scale, error)
+ax_error.plot(scale, ndarray(error) * 100, 'o-', linewidth=2, color='tab:blue', markersize=8)
 ax_error.set_xlabel('refinement')
-ax_error.set_ylabel('relative error')
-ax_error.set_xscale('log', base=2)
+ax_error.set_ylabel('relative error (%)')
 ax_error.grid(True)
-fig.savefig('refinement.pdf')
+
+ax_error = fig.add_subplot(212)
+error = [np.max(np.abs(u_n - u_nu[-1])) / u_nu_norm_max for u_n in u_nu]
+ax_error.plot(nu, ndarray(error) * 100, 'o-', linewidth=2, color='tab:blue', markersize=8)
+ax_error.set_xlabel('$\\nu$')
+ax_error.set_ylabel('relative error (%)')
+ax_error.grid(True)
+
+fig.savefig('refinement_left.pdf')
 plt.show()
