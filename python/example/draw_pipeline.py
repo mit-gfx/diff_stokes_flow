fb463e0d4f4d6e05c734c8ad755fabd2d2a045fa
Tao Du <taodu@csail.mit.edu>
2020-10-27 19:37:04 -0400

    Draw the complete pipeline.

--- python/example/draw_pipeline.py
+++ python/example/draw_pipeline.py
@@ -39,6 +39,7 @@ if __name__ == '__main__':
     face_color = ndarray([247 / 255, 247 / 255, 247 / 255])
     plt.rcParams['figure.facecolor'] = face_color
     plt.rcParams['axes.facecolor'] = face_color
+    padding = 5
 
     fig = plt.figure(figsize=(12, 10))
     ax = fig.add_subplot(111)
@@ -66,7 +67,6 @@ if __name__ == '__main__':
             d1 = sdf[i, j + 1]
             v0 = ndarray([i, j])
             v1 = ndarray([i, j + 1])
-            thin_lines.append((v0, v1))
             if d0 >= 0 and d1 >= 0:
                 lines.append((v0, v1))
             elif d0 * d1 < 0:
@@ -74,8 +74,12 @@ if __name__ == '__main__':
                 vt = (1 - t) * v0 + t * v1
                 if d0 > 0:
                     lines.append((v0, vt))
+                    thin_lines.append((vt, v1))
                 else:
                     lines.append((vt, v1))
+                    thin_lines.append((v0, vt))
+            else:
+                thin_lines.append((v0, v1))
 
     # Draw the interface.
     intercepts = []
@@ -90,18 +94,112 @@ if __name__ == '__main__':
         assert len(ds) == 2
         intercepts.append(ds)
 
+    interfaces = []
     for i in range(cell_nums[0]):
         for k in [0, 1]:
             v0 = ndarray([i, intercepts[i][k]])
             v1 = ndarray([i + 1, intercepts[i + 1][k]])
-            lines.append((v0, v1))
+            interfaces.append((v0, v1))
 
+    ax.add_collection(mc.LineCollection(interfaces, colors='k', linewidth=4.0))
     ax.add_collection(mc.LineCollection(thin_lines, colors='k', linewidth=1.0))
     ax.add_collection(mc.LineCollection(lines, colors='k', linewidth=4.0))
     ax.set_xticks([])
     ax.set_yticks([])
-    ax.set_xlim([-1, cell_nums[0] + 1])
-    ax.set_ylim([-1, cell_nums[1] + 1])
+    ax.set_xlim([-padding, cell_nums[0] + padding])
+    ax.set_ylim([-padding, cell_nums[1] + padding])
+    ax.axis('off')
 
     fig.savefig(folder / 'design.png')
+    plt.show()
+
+    # Draw figures for the governing equations.
+    fig = plt.figure(figsize=(12, 10))
+    ax = fig.add_subplot(111)
+
+    ax.add_collection(mc.LineCollection(interfaces, colors='tab:orange', linewidth=4.0))
+    ax.add_collection(mc.LineCollection(thin_lines, colors='tab:blue', linewidth=1.0))
+    ax.add_collection(mc.LineCollection(lines, colors='k', linewidth=4.0))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlim([-padding, cell_nums[0] + padding])
+    ax.set_ylim([-padding, cell_nums[1] + padding])
+    ax.axis('off')
+
+    fig.savefig(folder / 'governing_eqns.png')
+    plt.show()
+
+    # Draw the discretization figure.
+    fig = plt.figure(figsize=(12, 10))
+    ax = fig.add_subplot(111)
+
+    cell_edges = []
+    for i in range(cell_nums[0]):
+        for j in range(cell_nums[1]):
+            v00 = ndarray([i, j])
+            v10 = ndarray([i + 1, j])
+            v01 = ndarray([i, j + 1])
+            if j != 0:
+                cell_edges.append((v00, v10))
+            if i != 0:
+                cell_edges.append((v00, v01))
+
+    ax.add_collection(mc.LineCollection(cell_edges, colors='k', linewidth=1.0))
+    ax.add_collection(mc.LineCollection(interfaces, colors='tab:orange', linewidth=4.0))
+    ax.add_collection(mc.LineCollection(thin_lines, colors='tab:blue', linewidth=1.0))
+    ax.add_collection(mc.LineCollection(lines, colors='k', linewidth=4.0))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlim([-padding, cell_nums[0] + padding])
+    ax.set_ylim([-padding, cell_nums[1] + padding])
+    ax.axis('off')
+
+    fig.savefig(folder / 'discretization.png')
+    plt.show()
+
+    # Draw the simulation figure.
+    fig = plt.figure(figsize=(12, 10))
+    ax = fig.add_subplot(111)
+
+    # How to use cmap:
+    # cmap(0.0) to cmap(1.0) covers the whole range of the colormap.
+    cmap = plt.get_cmap('coolwarm')
+    velocities = []
+    velocity_colors = []
+    u_min = np.inf
+    u_max = -np.inf
+    for i in range(node_nums[0]):
+        for j in range(node_nums[1]):
+            uij = u[i, j]
+            uij_norm = np.linalg.norm(uij)
+            if uij_norm > 0:
+                if uij_norm > u_max:
+                    u_max = uij_norm
+                if uij_norm < u_min:
+                    u_min = uij_norm
+
+    for i in range(node_nums[0]):
+        for j in range(node_nums[1]):
+            uij = u[i, j]
+            uij_norm = np.linalg.norm(uij)
+            if uij_norm > 0:
+                v0 = ndarray([i, j])
+                v1 = v0 + uij
+                velocities.append((v0, v1))
+                # Determine the color.
+                color = cmap((uij_norm - u_min) / (u_max - u_min))
+                velocity_colors.append(color)
+
+    ax.add_collection(mc.LineCollection(velocities, colors=velocity_colors, linewidth=4.0))
+    ax.add_collection(mc.LineCollection(cell_edges, colors='k', linewidth=1.0))
+    ax.add_collection(mc.LineCollection(interfaces, colors='tab:orange', linewidth=4.0))
+    ax.add_collection(mc.LineCollection(thin_lines, colors='tab:blue', linewidth=1.0))
+    ax.add_collection(mc.LineCollection(lines, colors='k', linewidth=4.0))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlim([-padding, cell_nums[0] + padding])
+    ax.set_ylim([-padding, cell_nums[1] + padding])
+    ax.axis('off')
+
+    fig.savefig(folder / 'simulation.png')
     plt.show()
